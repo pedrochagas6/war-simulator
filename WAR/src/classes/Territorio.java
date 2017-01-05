c5b3f0103ddd140c1408e9d344509710b899c912
pedrochagas6 <pedrochagas6@icloud.com>
2017-01-05 15:42:12 -0200

    Add files via upload

---
+++ WAR/src/classes/Territorio.java
@@ -0,0 +1,37 @@
+package classes;
+
+public class Territorio {
+	
+	public enum Continente {
+		AFRICA,
+		AMERICA_DO_NORTE,
+		AMERICA_DO_SUL,
+		ASIA,
+		EUROPA,
+		OCEANIA;
+	}
+	
+	private String nome = new String();
+	private String cor = new String();
+	private Continente continente;
+	
+	public Territorio(Continente continente, String nome, String cor) {
+		super();
+		this.continente = continente;
+		this.nome = nome;
+		this.cor = cor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+}
